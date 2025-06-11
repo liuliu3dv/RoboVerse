f34496bce4e993de2b1bdb585ee8fe84dfed7808
wyzhgxy <148778499+wyzhgxy@users.noreply.github.com>
2025-06-11 14:45:10 +0800

    [fix] h1 cfg joints num error (#249)
    * isaacgym multi-robot
    
    * test_script
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [fix] align with isaaclab multirobot and support shadow handover
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [fix] h1 cfg joints num bug
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- get_started/7_multiple_robots.py
+++ get_started/7_multiple_robots.py
@@ -5,15 +5,22 @@ try:
 except ImportError:
     pass
 
+import os
 from dataclasses import dataclass
 
+import rootutils
 import torch
 import tyro
 from loguru import logger as log
 from rich.logging import RichHandler
 
+rootutils.setup_root(__file__, pythonpath=True)
+log.configure(handlers=[{"sink": RichHandler(), "format": "{message}"}])
+
+from get_started.utils import ObsSaver
 from metasim.cfg.robots import FrankaCfg, H1Cfg
 from metasim.cfg.scenario import ScenarioCfg
+from metasim.cfg.sensors import PinholeCameraCfg
 from metasim.constants import SimType
 from metasim.utils.setup_util import get_sim_env_class
 
@@ -44,7 +51,7 @@ def main():
         num_envs=args.num_envs,
         decimation=args.decimation,
     )
-
+    scenario.cameras = [PinholeCameraCfg(width=1024, height=1024, pos=(3.0, -3.0, 3.0), look_at=(0.0, -0.2, 0.0))]
     log.info(f"Using simulator: {args.sim}")
     env_class = get_sim_env_class(SimType(args.sim))
     env = env_class(scenario)
@@ -54,16 +61,23 @@ def main():
             "robots": {
                 "franka_1": {"pos": torch.tensor([1.0, 1.0, args.z_pos]), "rot": torch.tensor([1.0, 0.0, 0.0, 0.0])},
                 "franka_2": {"pos": torch.tensor([1.0, -1.0, args.z_pos]), "rot": torch.tensor([1.0, 0.0, 0.0, 0.0])},
-                "h1_1": {"pos": torch.tensor([-1.0, 1.0, args.z_pos]), "rot": torch.tensor([1.0, 0.0, 0.0, 0.0])},
-                "h1_2": {"pos": torch.tensor([-1.0, -1.0, args.z_pos]), "rot": torch.tensor([1.0, 0.0, 0.0, 0.0])},
+                "h1_1": {"pos": torch.tensor([-1.0, 1.0, args.z_pos + 1.0]), "rot": torch.tensor([1.0, 0.0, 0.0, 0.0])},
+                "h1_2": {
+                    "pos": torch.tensor([-1.0, -1.0, args.z_pos + 1.0]),
+                    "rot": torch.tensor([1.0, 0.0, 0.0, 0.0]),
+                },
             },
             "objects": {},
         }
     ] * scenario.num_envs
-    env.reset(states=init_states)
+    obs, extras = env.reset(states=init_states)
+
+    os.makedirs("get_started/output", exist_ok=True)
+    obs_saver = ObsSaver(video_path=f"get_started/output/7_multirobot_{args.sim}.mp4")
+    obs_saver.add(obs)
 
     step = 0
-    while True:
+    while step < 200:
         log.debug(f"Step {step}")
         actions = [
             {
@@ -81,11 +95,12 @@ def main():
             }
             for _ in range(scenario.num_envs)
         ]
-        env.step(actions)
-        env.render()
+        obs, reward, success, time_out, extras = env.step(actions)
+        obs_saver.add(obs)
         step += 1
 
     env.handler.close()
+    obs_saver.save()
 
 
 if __name__ == "__main__":
