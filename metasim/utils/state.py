liuliu3dv/RoboVerse

commit 7cf1a9782fa4b22892cb1bd68568891d20aeb34c

[add] isaaclab support segmentation output in camera state (#264)

metasim/utils/state.py
@@ -70,10 +70,29 @@ class RobotState:
 class CameraState:
     """State of a single camera."""
 
+    ## Images
     rgb: torch.Tensor | None
     """RGB image. Shape is (num_envs, H, W, 3)."""
     depth: torch.Tensor | None
     """Depth image. Shape is (num_envs, H, W)."""
+    instance_id_seg: torch.Tensor | None
+    """Instance id segmentation for each pixel. Shape is (num_envs, H, W)."""
+    instance_id_seg_id2label: dict[int, str] | None
+    """Instance id segmentation id to label mapping. Keys are instance ids, values are labels. Go together with :attr:`instance_id_seg`."""
+    instance_seg: torch.Tensor | None
+    """Instance segmentation for each pixel. Shape is (num_envs, H, W).
+
+    .. warning::
+        This is experimental and subject to change.
+    """
+    instance_seg_id2label: dict[int, str] | None
+    """Instance segmentation id to label mapping. Keys are instance ids, values are labels. Go together with :attr:`instance_seg`.
+
+    .. warning::
+        This is experimental and subject to change.
+    """
+
+    ## Camera parameters
     pos: torch.Tensor | None = None  # TODO: remove N
     """Position of the camera. Shape is (num_envs, 3)."""
     quat_world: torch.Tensor | None = None  # TODO: remove N

metasim/cfg/sensors/cameras.py
@@ -14,7 +14,7 @@ class BaseCameraCfg:
 
     name: str = "camera0"
     """Name of the camera. Defaults to "camera0". Different cameras should have different names, so if you add multiple cameras, make sure to give them unique names."""
-    data_types: list[Literal["rgb", "depth"]] = ["rgb", "depth"]
+    data_types: list[Literal["rgb", "depth", "instance_seg", "instance_id_seg"]] = ["rgb", "depth"]
     """List of sensor types to enable for the camera. Defaults to ["rgb", "depth"]."""
     width: int = 256
     """Width of the image in pixels. Defaults to 256."""

metasim/sim/isaaclab/isaaclab.py
@@ -18,6 +18,7 @@ from metasim.cfg.scenario import ScenarioCfg
 from metasim.cfg.sensors import ContactForceSensorCfg
 from metasim.sim import BaseSimHandler, EnvWrapper, IdentityEnvWrapper
 from metasim.types import Action, EnvState, Extra, Obs, Reward, Success, TimeOut
+from metasim.utils.dict import deep_get
 from metasim.utils.state import CameraState, ContactForceState, ObjectState, RobotState, TensorState
 
 from .env_overwriter import IsaaclabEnvOverwriter
@@ -385,9 +386,21 @@ class IsaaclabHandler(BaseSimHandler):
             camera_inst = self.env.scene.sensors[camera.name]
             rgb_data = camera_inst.data.output.get("rgb", None)
             depth_data = camera_inst.data.output.get("depth", None)
+            instance_seg_data = deep_get(camera_inst.data.output, "instance_segmentation_fast")
+            instance_seg_id2label = deep_get(camera_inst.data.info, "instance_segmentation_fast", "idToLabels")
+            instance_id_seg_data = deep_get(camera_inst.data.output, "instance_id_segmentation_fast")
+            instance_id_seg_id2label = deep_get(camera_inst.data.info, "instance_id_segmentation_fast", "idToLabels")
+            if instance_seg_data is not None:
+                instance_seg_data = instance_seg_data.squeeze(-1)
+            if instance_id_seg_data is not None:
+                instance_id_seg_data = instance_id_seg_data.squeeze(-1)
             camera_states[camera.name] = CameraState(
                 rgb=rgb_data,
                 depth=depth_data,
+                instance_seg=instance_seg_data,
+                instance_seg_id2label=instance_seg_id2label,
+                instance_id_seg=instance_id_seg_data,
+                instance_id_seg_id2label=instance_id_seg_id2label,
                 pos=camera_inst.data.pos_w,
                 quat_world=camera_inst.data.quat_w_world,
                 intrinsics=torch.tensor(camera.intrinsics, device=self.device)[None, ...].repeat(self.num_envs, 1, 1),

metasim/sim/isaaclab/isaaclab_helper.py
@@ -289,11 +289,18 @@ def _add_pinhole_camera(env: "EmptyEnv", camera: PinholeCameraCfg) -> None:
         import isaaclab.sim as sim_utils
         from isaaclab.sensors import TiledCamera, TiledCameraCfg
 
+    ## See https://isaac-sim.github.io/IsaacLab/main/source/api/lab/isaaclab.sensors.html#tile-rendered-usd-camera
+    data_type_map = {
+        "rgb": "rgb",
+        "depth": "depth",
+        "instance_seg": "instance_segmentation_fast",
+        "instance_id_seg": "instance_id_segmentation_fast",
+    }
     env.scene.sensors[camera.name] = TiledCamera(
         TiledCameraCfg(
             prim_path=f"/World/envs/env_.*/{camera.name}",
             offset=TiledCameraCfg.OffsetCfg(pos=(0.0, 0.0, 0.0), rot=(1.0, 0.0, 0.0, 0.0), convention="world"),
-            data_types=camera.data_types,
+            data_types=[data_type_map[dt] for dt in camera.data_types],
             spawn=sim_utils.PinholeCameraCfg(
                 focal_length=camera.focal_length,
                 focus_distance=camera.focus_distance,
@@ -302,6 +309,8 @@ def _add_pinhole_camera(env: "EmptyEnv", camera: PinholeCameraCfg) -> None:
             ),
             width=camera.width,
             height=camera.height,
+            colorize_instance_segmentation=False,
+            colorize_instance_id_segmentation=False,
         )
     )
 

metasim/utils/dict.py
@@ -9,6 +9,7 @@ from __future__ import annotations
 
 from collections.abc import Iterable, Mapping
 from dataclasses import is_dataclass
+from functools import reduce
 from typing import Any
 
 from .string_util import callable_to_string, string_to_callable, string_to_dataclass_instance
@@ -16,6 +17,14 @@ from .string_util import callable_to_string, string_to_callable, string_to_datac
 """
 Dictionary <-> Class operations.
 """
+
+
+def deep_get(dictionary, *keys):
+    """Get a value from a dictionary using a list of keys. If any key is not found, return None.
+
+    Refer to https://stackoverflow.com/a/36131992/30487149.
+    """
+    return reduce(lambda d, key: d.get(key) if d else None, keys, dictionary)
 
 
 def class_to_dict(obj: object) -> dict[str, Any]:

commit 00b186329d3945059e812b428edaf3ea0b52bced

[bugfix] camera state bug introduced in #264

metasim/utils/state.py
@@ -75,17 +75,17 @@ class CameraState:
     """RGB image. Shape is (num_envs, H, W, 3)."""
     depth: torch.Tensor | None
     """Depth image. Shape is (num_envs, H, W)."""
-    instance_id_seg: torch.Tensor | None
+    instance_id_seg: torch.Tensor | None = None
     """Instance id segmentation for each pixel. Shape is (num_envs, H, W)."""
-    instance_id_seg_id2label: dict[int, str] | None
+    instance_id_seg_id2label: dict[int, str] | None = None
     """Instance id segmentation id to label mapping. Keys are instance ids, values are labels. Go together with :attr:`instance_id_seg`."""
-    instance_seg: torch.Tensor | None
+    instance_seg: torch.Tensor | None = None
     """Instance segmentation for each pixel. Shape is (num_envs, H, W).
 
     .. warning::
         This is experimental and subject to change.
     """
-    instance_seg_id2label: dict[int, str] | None
+    instance_seg_id2label: dict[int, str] | None = None
     """Instance segmentation id to label mapping. Keys are instance ids, values are labels. Go together with :attr:`instance_seg`.
 
     .. warning::