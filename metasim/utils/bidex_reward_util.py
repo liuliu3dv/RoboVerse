9ca5cd58f46fc13034bee5aa7cbf74ac04b91368
wyzhgxy <2200012805@stu.pku.edu.cn>
2025-06-16 20:12:19 +0800

    [add] bidex shadow hand over task cfg, observation and reward included

---
+++ metasim/utils/bidex_reward_util.py
@@ -0,0 +1,88 @@
+"""Sub-module containing utilities for computing bidex shadow hand rewards."""
+
+import torch
+
+from metasim.utils.math import quat_inv, quat_mul
+
+
+@torch.jit.script
+def compute_hand_reward(
+    reset_buf,
+    reset_goal_buf,
+    _episode_length_buf,
+    max_episode_length: float,
+    object_pos,
+    object_rot,
+    target_pos,
+    target_rot,
+    dist_reward_scale: float,
+    actions,
+    success_tolerance: float,
+    reach_goal_bonus: float,
+    fall_penalty: float,
+    ignore_z_rot: bool,
+):
+    """Compute the reward of all environment.
+
+    Args:
+        reset_buf (tensor): The reset buffer of all environments at this time, shape (num_envs,)
+
+        reset_goal_buf (tensor): The reset goal buffer of all environments at this time, shape (num_envs,)
+
+        _episode_length_buf (tensor): The porgress buffer of all environments at this time, shape (num_envs,)
+
+        max_episode_length (float): The max episode length in this environment
+
+        object_pos (tensor): The position of the object
+
+        object_rot (tensor): The rotation of the object
+
+        target_pos (tensor): The position of the target
+
+        target_rot (tensor): The rotate of the target
+
+        dist_reward_scale (float): The scale of the distance reward
+
+        actions (tensor): The action buffer of all environments at this time
+
+        success_tolerance (float): The tolerance of the success determined
+
+        reach_goal_bonus (float): The reward given when the object reaches the goal
+
+        fall_penalty (float): The reward given when the object is fell
+
+        ignore_z_rot (bool): Is it necessary to ignore the rot of the z-axis, which is usually used
+            for some specific objects (e.g. pen)
+    """
+    # Distance from the hand to the object
+    goal_dist = torch.norm(target_pos - object_pos, p=2, dim=-1)
+    if ignore_z_rot:
+        success_tolerance = 2.0 * success_tolerance
+
+    # Orientation alignment for the cube in hand and goal cube
+    quat_diff = quat_mul(object_rot, quat_inv(target_rot))
+    rot_dist = 2.0 * torch.asin(torch.clamp(torch.norm(quat_diff[:, 1:4], p=2, dim=-1), max=1.0))
+
+    dist_rew = goal_dist
+
+    action_penalty = torch.sum(actions**2, dim=-1)
+
+    # Total reward is: position distance + orientation alignment + action regularization + success bonus + fall penalty
+    reward = torch.exp(-0.2 * (dist_rew * dist_reward_scale + rot_dist))
+
+    # Find out which envs hit the goal and update successes count
+    goal_resets = torch.where(torch.abs(goal_dist) <= 0, torch.ones_like(reset_goal_buf), reset_goal_buf)
+
+    # Success bonus: orientation is within `success_tolerance` of goal orientation
+    reward = torch.where(goal_resets == 1, reward + reach_goal_bonus, reward)
+
+    # Fall penalty: distance to the goal is larger than a threashold
+    reward = torch.where(object_pos[:, 2] <= 0.2, reward + fall_penalty, reward)
+
+    # Check env termination conditions, including maximum success number
+    resets = torch.where(object_pos[:, 2] <= 0.2, torch.ones_like(reset_buf), reset_buf)
+
+    # Only reset because of terminate or fall
+    resets = torch.where(_episode_length_buf >= max_episode_length, torch.ones_like(resets), resets)
+
+    return reward, resets, goal_resets
