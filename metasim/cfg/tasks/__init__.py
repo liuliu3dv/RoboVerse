9e8406327497d40038b5ee6fbad76835cdbf4b5f
wyzhgxy <2200012805@stu.pku.edu.cn>
2025-07-05 19:05:30 +0800

    [add] shadow hand catch abreast added

--- metasim/cfg/tasks/__init__.py
+++ metasim/cfg/tasks/__init__.py
@@ -15,6 +15,8 @@ def __get_quick_ref():
     from .bidex.shadow_hand_catch_underarm_cfg import ShadowHandCatchUnderarmCfg
     from .bidex.shadow_hand_over_cfg import ShadowHandOverCfg
     from .bidex.shadow_hand_over2underarm_cfg import ShadowHandOver2UnderarmCfg
+    from .bidex.shadow_hand_push_block_cfg import ShadowHandPushBlockCfg
+    from .bidex.shadow_hand_catch_abreast_cfg import ShadowHandCatchAbreastCfg
     from .calvin.calvin import MoveSliderLeftACfg
 
     # from .debug.reach_origin_cfg import ReachOriginCfg
