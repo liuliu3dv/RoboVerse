06b99391ba5b165d5563907d29b3e8792bdb9630
XyC0212 <cxy980212@163.com>
2025-08-06 20:15:35 +0800

    add robot

---
+++ roboverse_data/robots/g1/urdf/test.py
@@ -0,0 +1,3 @@
+from mjcf_urdf_simple_converter import convert
+convert('/home/xyc/RoboVerse/roboverse_data/robots/g1/urdf/g1_29dof.xml',
+        '/home/xyc/RoboVerse/roboverse_data/robots/g1/urdf/model.urdf')
