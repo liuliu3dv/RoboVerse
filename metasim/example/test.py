3b8a92dac15a6e460e0c561141d0cc0313b932ed
MurphyZhao04 <xh930717304@gmail.com>
2025-08-18 21:58:54 -0700

    add example test

--- metasim/example/test.py
+++ metasim/example/test.py
@@ -52,13 +52,12 @@ if __name__ == "__main__":
         num_envs=args.num_envs,
     )
 
-    # add cameras
-    scenario.cameras = [PinholeCameraCfg(width=1024, height=1024, pos=(1.5, -1.5, 1.5), look_at=(0.0, 0.0, 0.0))]
-
     log.info(f"Using simulator: {args.sim}")
     task_cls = get_task_class("obj_env")
     # Get default scenario from task class and update with specific parameters
     scenario = task_cls.scenario.update(simulator=args.sim, num_envs=args.num_envs, headless=args.headless, cameras=[])
+    # add cameras
+    scenario.cameras = [PinholeCameraCfg(width=1024, height=1024, pos=(1.5, -1.5, 1.5), look_at=(0.0, 0.0, 0.0))]
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     log.info(f"Using device: {device}")
@@ -90,7 +89,9 @@ if __name__ == "__main__":
             }
             for _ in range(scenario.num_envs)
         ]
-        obs, reward, terminated, time_out, info = env.step(actions)
+        flatten_obs, reward, terminated, time_out, info = env.step(actions)
+        raw_obs = env.handler.get_states()
+        obs_saver.add(raw_obs)
         step += 1
 
     obs_saver.save()
