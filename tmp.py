2ef35c1c2101fb49c23afe8ffd5b04fda06fa782
Feishi Wang <feishiwang2001@gmail.com>
2025-08-24 02:09:00 -0700

    polish

--- tmp.py
+++ tmp.py
@@ -64,37 +64,6 @@ scenario = ScenarioCfg(
 # add cameras
 scenario.cameras = [PinholeCameraCfg(width=256, height=256, pos=(1.5, -1.5, 1.5), look_at=(0.0, 0.0, 0.0))]
 
-# add objects
-scenario.objects = [
-    PrimitiveCubeCfg(
-        name="cube",
-        size=(0.1, 0.1, 0.1),
-        color=[1.0, 0.0, 0.0],
-        physics=PhysicStateType.RIGIDBODY,
-    ),
-    PrimitiveSphereCfg(
-        name="sphere",
-        radius=0.1,
-        color=[0.0, 0.0, 1.0],
-        physics=PhysicStateType.RIGIDBODY,
-    ),
-    RigidObjCfg(
-        name="bbq_sauce",
-        scale=(2, 2, 2),
-        physics=PhysicStateType.RIGIDBODY,
-        usd_path="get_started/example_assets/bbq_sauce/usd/bbq_sauce.usd",
-        urdf_path="get_started/example_assets/bbq_sauce/urdf/bbq_sauce.urdf",
-        mjcf_path="get_started/example_assets/bbq_sauce/mjcf/bbq_sauce.xml",
-    ),
-    ArticulationObjCfg(
-        name="box_base",
-        fix_base_link=True,
-        usd_path="get_started/example_assets/box_base/usd/box_base.usd",
-        urdf_path="get_started/example_assets/box_base/urdf/box_base_unique.urdf",
-        mjcf_path="get_started/example_assets/box_base/mjcf/box_base_unique.mjcf",
-    ),
-]
-
 
 log.info(f"Using simulator: {args.sim}")
 env_class = get_sim_env_class(SimType(args.sim))
