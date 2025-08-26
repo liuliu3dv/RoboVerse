2038685a2255f77808fdf723e064bfe462e41cfb
JIAjindou <jdjia@buaa.edu.cn>
2025-08-26 08:09:34 +0000

    add scored-based diffusion

--- roboverse_learn/il/run.sh
+++ roboverse_learn/il/run.sh
@@ -17,7 +17,7 @@ train_enable=True
 eval_enable=True
 
 ## Choose training or inference algorithm
-algo_choose=0  # 0: DDPM, 1: DDIM, 2: FM
+algo_choose=3  # 0: DDPM, 1: DDIM, 2: FM  3: Score-based    
 
 algo_model=""
 eval_path=""
