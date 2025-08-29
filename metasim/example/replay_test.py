31ee795279b179b96c833a59e5db44f574f4c6f4
MurphyZhao04 <xh930717304@gmail.com>
2025-08-29 03:41:32 -0700

    add pick cube task

--- metasim/example/replay_test.py
+++ metasim/example/replay_test.py
@@ -38,7 +38,7 @@ log.configure(handlers=[{"sink": RichHandler(), "format": "{message}"}])
 
 @configclass
 class Args:  # noqa: D101
-    task: str = "close_box"
+    task: str = "pick_cube"
     robot: str = "franka"
     scene: str | None = None
     render: RenderCfg = RenderCfg()
