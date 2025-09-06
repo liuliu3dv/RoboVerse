f7a2d261120ad66dae7eb1db02c1aced82639e37
MurphyZhao04 <xh930717304@gmail.com>
2025-09-06 04:36:57 -0700

    [fix] rl bench reset

--- roboverse_pack/tasks/rlbench/rl_bench.py
+++ roboverse_pack/tasks/rlbench/rl_bench.py
@@ -32,10 +32,7 @@ class RLBenchTask(BaseTaskEnv):
 
     def _get_initial_states(self) -> list[dict] | None:
         """Give the inital states from traj file."""
-        # Keep it simple and leave robot states to defaults; just seed cube pose.
-        # If the handler handles None gracefully, this can be set to None.
         initial_states, _, _ = get_traj(self.traj_filepath, self.scenario.robots[0], self.handler)
-        # Duplicate / trim list so that its length matches num_envs
         if len(initial_states) < self.num_envs:
             k = self.num_envs // len(initial_states)
             initial_states = initial_states * k + initial_states[: self.num_envs % len(initial_states)]
@@ -43,5 +40,7 @@ class RLBenchTask(BaseTaskEnv):
         return self._initial_states
 
     def reset(self, states=None, env_ids=None):
-        self.checker.reset(self.handler)
-        return super().reset(states, env_ids)
+        """Reset the checker."""
+        states = super().reset(states, env_ids)
+        self.checker.reset(self.handler, env_ids=env_ids)
+        return states
